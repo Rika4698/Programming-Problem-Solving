d606a61a6f808517e4de3b96e487f38db65ceb3e
Rika4698 <rikasharmin181@gmail.com>
2025-06-02 01:08:14 +0600

    Ideal Generator

---
+++ 2093 A. Ideal Generator.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+
+    int t ;
+    cin>>t;
+    while(t--){
+        int k;
+        cin >> k;
+        if(k%2 == 1){
+            cout << "Yes" <<endl;
+        }
+        else{
+            cout << "No" <<endl;
+        }
+    }
+    return 0;
+
+}
